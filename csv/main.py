b5e62d2fe5e278199bdb6e079c2fb62f9b2a39fe
Saran <46473540+sarannetworkprogammer@users.noreply.github.com>
2023-07-20 23:20:17 -0700

    CSV

---
+++ csv/main.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+import pandas as pd
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
+import matplotlib
+
+
+matplotlib.use('Tkagg')
+
+
+load_dotenv()
+
+API_KEY = os.environ["OPENAI_API_KEY"]
+
+
+llm = OpenAI(api_token = API_KEY)
+pandas_ai = PandasAI(llm)
+
+st.title("Aruba Intelligence-Prompt-driven analysis")
+
+
+with st.container():
+
+    uploaded_file = st.file_uploader("Upload a CSV file for Analysis", type=['csv'])
+
+
+    if uploaded_file is not None:
+
+        df = pd.read_csv(uploaded_file)
+        st.write(df.head(3))
+
+
+        prompt = st.text_area("Enter your  prompt")
+
+        if st.button("Generate"):
+            if prompt:
+                with st.spinner("Generating response...."):
+                    st.write(pandas_ai.run(df,prompt=prompt))
+                            
+            else:
+            
+                st.warning("Please enter a prompt")
+
+
+
+
